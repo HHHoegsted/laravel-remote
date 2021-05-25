fc73ccaad04b9842bc0d50b23e9e5b3ec6016d42
Hans-Henrik Høgsted <hh@debtia.dk>
2021-05-25 13:00:00 +0200

    FEATURE: First iteration works

--- config/remote.php
+++ config/remote.php
@@ -1,5 +1,12 @@
 <?php
 // config for Hhhoegsted/ClassName
 return [
-
+	'hosts' => [
+		'default' => [
+			'host' => env('REMOTE_ENV'),
+			'user' => env('REMOTE_USER'),
+			'port' => env('REMOTE_PORT', 22),
+			'path' => env('REMOTE_PATH'),
+		]
+	]
 ];
--- src/Commands/LaravelRemoteCommand.php
+++ src/Commands/LaravelRemoteCommand.php
@@ -2,16 +2,27 @@
 
 namespace Hhhoegsted\LaravelRemote\Commands;
 
+use Spatie\Ssh\Ssh;
 use Illuminate\Console\Command;
 
 class LaravelRemoteCommand extends Command
 {
-    public $signature = 'remote {command}';
+	public $signature = 'remote {rawCommand}';
 
-    public $description = 'Execute commands on a remote server';
+	public $description = 'Execute commands on a remote server';
 
-    public function handle()
-    {
-        $this->comment('All done');
-    }
+	protected function getCommand() : string
+	{
+		return $this->argument('rawCommand');
+	}
+
+	public function handle()
+	{
+		Ssh::create('user', 'example.com')
+				->onOutput(function($type, $line) {
+					echo $line;
+				})
+				->execute($this->getCommand());
+
+	}
 }
